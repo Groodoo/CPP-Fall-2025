e25975528dc3e807b9c4b2ec3580682c54b36370
teseoch <teseo.schneider@gmail.com>
2025-09-04 10:37:12 -0700

    codes

---
+++ codes/stopwatch.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <chrono>
+
+class Stopwatch
+{
+public:
+    Stopwatch() = default;
+
+    void start()
+    {
+        start_time = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop()
+    {
+        end_time = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed()
+    {
+    }
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+    std::chrono::time_point<std::chrono::high_resolution_clock> end_time;
+};
